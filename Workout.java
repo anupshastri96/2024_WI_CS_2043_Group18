a512944377d8b4742838c35c0fc239c035505fcb
sandihnin <khinsandihnin2712@gmail.com>
2024-03-30 13:52:29 -0300

    Update Workout.java

--- Workout.java
+++ Workout.java
@@ -4,13 +4,41 @@ public class Workout {
     private String type;
     private int durationMinutes;
     private Date date;
+    private int intensity; 
+    private String location;
+    private String notes;
 
-    public Workout(String type, int durationMinutes, Date date) {
+    public Workout(String type, int durationMinutes, Date date, int intensity, String location, String notes) {
         this.type = type;
         this.durationMinutes = durationMinutes;
         this.date = date;
+        this.intensity = intensity;
+        this.location = location;
+        this.notes = notes;
+    }
+    public int getIntensity() {
+        return intensity;
     }
 
+    public void setIntensity(int intensity) {
+        this.intensity = intensity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
     public String getType() {
         return type;
     }
