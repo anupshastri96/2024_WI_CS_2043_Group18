5f1fe4d73c5e4a27a612b64e03fc4c365593ab5b
sandihnin <khinsandihnin2712@gmail.com>
2024-03-30 13:49:16 -0300

    Update CalorieIntake.java

--- CalorieIntake.java
+++ CalorieIntake.java
@@ -3,10 +3,14 @@ import java.util.Date;
 public class CalorieIntake {
     private int calories;
     private Date date;
+    private String mealType;
+    private String description;
 
-    public CalorieIntake(int calories, Date date) {
+    public CalorieIntake(int calories, Date date, String mealType, String description) {
         this.calories = calories;
         this.date = date;
+        this.mealType = mealType;
+        this.description = description;
     }
     
     public int getCalories() {
@@ -23,5 +27,25 @@ public class CalorieIntake {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public String getMealType() {
+        return mealType;
+    }
+
+    public void setMealType(String mealType) {
+        this.mealType = mealType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public String toString() {
+        return "Calorie Intake: " + calories + " calories, Date: " + date + ", Meal Type: " + mealType + ", Description: " + description;
     }
 }
